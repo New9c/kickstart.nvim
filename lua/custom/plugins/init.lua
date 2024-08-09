9c31ae335d14f70cde3248d811121bfa2aaf2c35
9c <ericgwhuang@gmail.com>
2024-08-09 16:30:18 +0800

    File Tree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -28,14 +28,32 @@ return {
     opts = {},
     -- stylua: ignore
     keys = {
-      { "n", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "<leader>n", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
       { "<leader>l", mode = { "n", "x", "o" }, function()
         require("flash").jump({
         search = { mode = "search", max_length = 0 },
         label = { after = { 0, 0 } },
         pattern = "^"
       }) end, desc = "Line Jumper" },
-    }
-,
+    },
+  },
+  {
+    'nvim-tree/nvim-tree.lua',
+    opts = {
+      on_attach = function(bufnr)
+        local api = require 'nvim-tree.api'
+        local function opts(desc)
+          return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+        -- default mappings
+        api.config.mappings.default_on_attach(bufnr)
+        -- custom mappings
+        vim.keymap.set('n', '<CR>', api.tree.change_root_to_node, opts 'CD')
+        vim.keymap.set('n', "'", api.tree.change_root_to_parent, opts 'Up')
+      end,
+    },
+    keys = {
+      { '<leader>t', ':NvimTreeOpen<CR>', mode = 'n', silent = true, desc = 'File tree' },
+    },
   },
 }
